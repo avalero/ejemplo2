3bfa0cb6ab177db112a65349444ddd84e5858119
Alberto Valero <alberto.valero@bq.com>
2024-06-19 11:26:08 +0200

    personas lambda

--- lambda/main.cpp
+++ lambda/main.cpp
@@ -1,82 +1,110 @@
 #include <functional>
 #include <iostream>
 #include <vector>
+#include <memory>
 
 using namespace std;
+
+struct Person
+{
+  string name;
+  int edad;
+};
+
+typedef vector<shared_ptr<Person>> vP;
+typedef function<void(shared_ptr<Person>)> voidFunPerson;
+typedef function<bool(shared_ptr<Person>)> boolFunPerson;
+typedef function<shared_ptr<Person>(shared_ptr<Person>)> PersonFunPerson;
 
 typedef vector<int> vi;
 typedef function<void(int)> flambda;
 typedef function<bool(int)> boollambda;
 
-// for Each --> recorre un vector y hace "algo" con cada uno de sus elementos
-void forEach(vi const &v, flambda const &f)
+void forEachPersons(vP const &v, voidFunPerson const &f)
 {
-  for (auto elem : v)
+  for (auto const &elem : v)
   {
     f(elem);
   }
 }
-
-// some --> recibe un vector y una funcion lambda y devuelva true si algun elemento del vector cumple el requisito de la función
-bool some(vi const &v, function<bool(int)> const &f)
+vP filterPersons(vP const &v, boolFunPerson const &f)
 {
-  for (auto elem : v)
+  vP result;
+  for (auto const &elem : v)
+  {
+    if (f(elem))
+      result.push_back(elem);
+  }
+  return result;
+}
+bool somePersons(vP const &v, boolFunPerson const &f)
+{
+  for (auto const &elem : v)
   {
     if (f(elem))
       return true;
   }
   return false;
 }
-
-bool every(vi const &v, function<bool(int)> const &f)
+bool everyPersons(vP const &v, boolFunPerson const &f)
 {
-  for (auto elem : v)
+  for (auto const &elem : v)
   {
     if (!f(elem))
       return false;
   }
-
   return true;
 }
-
-// v -> {1,2,3,4}, f -> mayor que 2    -----> {3,4}
-vector<int> filter(vector<int> const &v, function<bool(int)> const &f)
+vP transformPerson(vP const &v, PersonFunPerson const &f)
 {
+  vP result;
+  for (auto elem : v)
+  {
+    // result.push_back((f(elem)));
+    shared_ptr<Person> copiaDeLaPersona = make_shared<Person>(*elem);
+    result.push_back(f(copiaDeLaPersona));
+  }
+  return result;
 }
 
-// v -> {1,2,3,4}, f --> *2   -----> {2,4,6,8}
-vector<int> transform(vector<int> const &v, function<int(int)> const &f)
-{
-}
 int main()
 {
-  flambda print = [](int a)
-  { cout << a; };
+  vP personas{
+      make_shared<Person>(Person{"Alberto", 19}),
+      make_shared<Person>(Person{"José", 12}),
+      make_shared<Person>(Person{"Juan", 42}),
+      make_shared<Person>(Person{"Luis", 13}),
+      make_shared<Person>(Person{"María", 65}),
+      make_shared<Person>(Person{"Lucía", 19})};
 
-  boollambda gt10 = [](int a)
-  {
-    return a > 10;
-  };
+  forEachPersons(personas, [](shared_ptr<Person> p)
+                 { cout << p->name << endl; });
 
-  vi miVector{1, 2, 3, 4, 5, 6};
+  forEachPersons(personas, [](shared_ptr<Person> p)
+                 { if(p->edad >= 18) cout << p->name << endl; });
 
-  forEach(miVector, print);
-  cout << endl;
+  auto mayoresDe20 = filterPersons(personas, [](shared_ptr<Person> p)
+                                   { return p->edad > 20; });
 
-  if (some(miVector, gt10))
-    cout << "Algun elemento es mayor que 10" << endl;
-  else
-    cout << "Todos son menores que 10" << endl;
+  forEachPersons(mayoresDe20, [](shared_ptr<Person> p)
+                 { cout << p->name << endl; });
 
-  if (some(miVector, [](int a)
-           { return a > 10; }))
-  {
-    cout << "Alguno es mayor que 10" << endl;
-  }
+  bool existeAlberto = somePersons(personas, [](shared_ptr<Person> p)
+                                   { return p->name == "Alberto"; });
+  if (existeAlberto)
+    cout << "Hay algun Alberto" << endl;
 
-  some(miVector, [](int a)
-       { return true; });
+  bool todosMayoresDe15 = everyPersons(personas, [](shared_ptr<Person> p)
+                                       { return p->edad > 15; });
 
-  forEach(miVector, [](int elem)
-          { cout << 2 * elem; });
+  if (todosMayoresDe15)
+    cout << "Todos son mayores de 15" << endl;
+
+  auto todosMasViejos = transformPerson(personas, [](shared_ptr<Person> p)
+                                        { 
+                                          p->edad++;
+                                          return p; });
+
+  for (auto p : todosMasViejos)
+    cout << p->name;
 }
