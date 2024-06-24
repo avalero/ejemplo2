97581c767cbe9e0773ad3fb2a00a8aaa399d41fc
Alberto Valero <alberto.valero@bq.com>
2024-06-24 12:11:50 +0200

    ...

--- listas/main.cpp
+++ listas/main.cpp
@@ -1,6 +1,7 @@
 #include <memory>
 #include <iostream>
 #include <functional>
+#include <vector>
 
 using namespace std;
 
@@ -83,6 +84,22 @@ void forEach(P_Node<T> first, function<void(P_Node<T>)> f)
 }
 
 template <typename T>
+vector<P_Node<T>> filter(P_Node<T> first, function<bool(P_Node<T>)> f)
+{
+  vector<P_Node<T>> result;
+  auto it = first;
+  while (it)
+  {
+    if (f(it))
+    {
+      result.push_back(it);
+    }
+    it = it->next;
+  }
+  return result;
+}
+
+template <typename T>
 void remove(P_Node<T> &head, string const &id)
 {
   if (head->id == id)
@@ -98,28 +115,116 @@ void remove(P_Node<T> &head, string const &id)
   it->next = it->next->next;
 }
 
+struct Person
+{
+  string nombre;
+  string telefono;
+};
+
+void showByName(P_Node<Person> first)
+{
+
+  string name;
+  cout << "Introduce nombre: ";
+  getline(cin, name);
+  auto result = filter<Person>(first, [name](P_Node<Person> p)
+                               { return p->data.nombre.find(name) != -1; });
+  cout << "\n-----------------------------------------------------------------------\n";
+  for (auto const &p : result)
+  {
+    cout << "|\t" << p->id << ":\t" << p->data.nombre << "\t" << p->data.telefono << endl;
+  }
+  cout << "\n-----------------------------------------------------------------------\n";
+}
+
+void showAll(P_Node<Person> const &first)
+{
+  cout << "\n-----------------------------------------------------------------------\n";
+  forEach<Person>(first, [](P_Node<Person> p)
+                  { cout << "|\t" << p->id << ":\t" << p->data.nombre << "\t" << p->data.telefono << endl; });
+  cout << "-----------------------------------------------------------------------\n";
+}
+
+void add(P_Node<Person> &first)
+{
+  string nombre;
+  string telefono;
+  cout << "Introduce nombre: ";
+  getline(cin, nombre);
+  cout << "Introduce telefono: ";
+  getline(cin, telefono);
+  if (first == nullptr)
+  {
+    first = make_shared<Node<Person>>(Node<Person>{{nombre, telefono}, nullptr, uuid()});
+  }
+  else
+  {
+    push<Person>(first, {nombre, telefono});
+  }
+}
+
+void removeById(P_Node<Person> &first)
+{
+  string id;
+  cout << "Introduce id: ";
+  cin >> id;
+  cin.ignore();
+  remove<Person>(first, id);
+}
+
 int main()
 {
+
+  // intialize seed
+  srand(time(0));
 
   // 1. Mostrar todos (id, nombre, telefon)
   // 2. Añadir (nombre, telefono)
   // 3. Borrar (por id)
   // 4. Buscar (por nombre) -> mostrar
 
-  // intialize seed
-  srand(time(0));
+  P_Node<Person> head = nullptr;
 
-  P_Node<int> start = make_shared<Node<int>>(Node<int>{1, nullptr, uuid()});
-  push(start, 23);
-  push(start, 31);
-  push(start, 45);
-  cout << "Iterativo: " << endl;
-  imprimir_elems<int>(start);
-
-  cout << "\n\nRecursivo: " << endl;
-  imprimir_recursivo<int>(start);
-
-  cout << "\n\nFuncional: " << endl;
-  forEach<int>(start, [](P_Node<int> p)
-               { cout << p->id << ":\t" << p->data << endl; });
+  int option{0};
+  while (true)
+  {
+    // clear screen
+    if (option != 0)
+    {
+      cout << "Pulsa enter para continuar... ";
+      cin.get();
+    }
+    cout << "\033[2J\033[1;1H";
+    cout << "╔════════════════════════════════════════════╗" << endl;
+    cout << "║                 MENU PRINCIPAL             ║" << endl;
+    cout << "╠════════════════════════════════════════════╣" << endl;
+    cout << "║  1. Mostrar todos (id, nombre, telefono)   ║" << endl;
+    cout << "║  2. Añadir (nombre, telefono)              ║" << endl;
+    cout << "║  3. Borrar (por id)                        ║" << endl;
+    cout << "║  4. Buscar (por nombre) -> mostrar         ║" << endl;
+    cout << "║ -1. Salir                                  ║" << endl;
+    cout << "╚════════════════════════════════════════════╝" << endl;
+    cout << "Introduce una opción: ";
+    cin >> option;
+    cin.ignore();
+    switch (option)
+    {
+    case 1:
+      showAll(head);
+      break;
+    case 2:
+      add(head);
+      break;
+    case 3:
+      removeById(head);
+      break;
+    case 4:
+      showByName(head);
+      break;
+    case -1:
+      return 0;
+    default:
+      cout << "Opción no válida" << endl;
+    }
+  }
 }
