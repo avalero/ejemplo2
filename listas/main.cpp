8cc712197c99a86d8cf5bcda28e80de08571f790
Alberto Valero <alberto.valero@bq.com>
2024-06-24 10:48:50 +0200

    print

--- listas/main.cpp
+++ listas/main.cpp
@@ -20,7 +20,7 @@ struct Node
 {
   T data;
   shared_ptr<Node> next;
-  // string id;
+  string id;
 };
 
 template <typename T>
@@ -34,7 +34,7 @@ P_Node<T> push(P_Node<T> first, T const &data)
   {
     it = it->next;
   }
-  P_Node<T> nNode = make_shared<Node<T>>(Node<T>{data, nullptr});
+  P_Node<T> nNode = make_shared<Node<T>>(Node<T>{data, nullptr, uuid()});
   it->next = nNode;
   return nNode;
 }
@@ -45,7 +45,7 @@ P_Node<T> push_r(P_Node<T> first, T const &data)
   if (first->next)
     return push_r<T>(first->next, data);
 
-  P_Node<T> nNode = make_shared<Node<T>>(Node<T>{data, nullptr});
+  P_Node<T> nNode = make_shared<Node<T>>(Node<T>{data, nullptr, uuid()});
   first->next = nNode;
   return nNode;
 }
@@ -56,7 +56,7 @@ void imprimir_elems(P_Node<T> first)
   auto it = first;
   while (it)
   {
-    cout << it->data << endl;
+    cout << it->id << ":\t" << it->data << endl;
     it = it->next;
   }
 }
@@ -66,27 +66,38 @@ void imprimir_recursivo(P_Node<T> p)
 {
   if (p)
   {
-    cout << p;
+    cout << p->id << ":\t" << p->data << endl;
     imprimir_recursivo<T>(p->next);
   }
 }
 
 template <typename T>
-void forEach(P_Node<T> first, fuction<void(T)> f)
+void forEach(P_Node<T> first, function<void(P_Node<T>)> f)
 {
   auto it = first;
   while (it)
   {
-    f(it->data);
+    f(it);
     it = it->next;
   }
 }
 
 int main()
 {
+  // intialize seed
+  srand(time(0));
 
-  P_Node<int> start = make_shared<Node<int>>(Node<int>{1, nullptr});
-  auto segundo = push(start, 23);
-  auto tercero = push(segundo, 31);
-  push(tercero, 45);
+  P_Node<int> start = make_shared<Node<int>>(Node<int>{1, nullptr, uuid()});
+  push(start, 23);
+  push(start, 31);
+  push(start, 45);
+  cout << "Iterativo: " << endl;
+  imprimir_elems<int>(start);
+
+  cout << "\n\nRecursivo: " << endl;
+  imprimir_recursivo<int>(start);
+
+  cout << "\n\nFuncional: " << endl;
+  forEach<int>(start, [](P_Node<int> p)
+               { cout << p->id << ":\t" << p->data << endl; });
 }
