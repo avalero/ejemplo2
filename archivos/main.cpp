3e1891d6fb1ec635ffe1a03f2dc3f9465c9c96a6
Alberto Valero <alberto.valero@bq.com>
2024-06-26 09:30:17 +0200

    matrices

--- archivos/main.cpp
+++ archivos/main.cpp
@@ -1,67 +1,79 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-
+#include <array>
 using namespace std;
 
-struct Point
+struct Matrix
 {
-  float x;
-  float y;
+  array<array<float, 3>, 3> data;
 };
 
-Point operator+(Point const &a, Point const &b)
+istream &operator>>(istream &is, Matrix &m)
 {
-  return Point({a.x + b.x, a.y + b.y});
-}
-
-ostream &operator<<(ostream &os, Point const &p)
-{
-  os << "(" << p.x << " , " << p.y << ")";
-  return os;
-}
-
-istream &operator>>(istream &is, Point &p)
-{
-  int x, y;
-  is >> p.x >> p.y;
+  for (int r{0}; r < m.data.size(); r++)
+  {
+    for (int c{0}; c < m.data.at(r).size(); c++)
+    {
+      is >> m.data.at(r).at(c);
+    }
+  }
   return is;
+}
+
+ostream &operator<<(ostream &os, Matrix const &m)
+{
+  if (typeid(os) == typeid(ofstream))
+  {
+    for (int r{0}; r < m.data.size(); r++)
+    {
+      for (int c{0}; c < m.data.at(r).size(); c++)
+      {
+        os << m.data.at(r).at(c) << " ";
+      }
+    }
+  }
+  else
+  {
+    for (int r{0}; r < m.data.size(); r++)
+    {
+      for (int c{0}; c < m.data.at(r).size(); c++)
+      {
+        os << m.data.at(r).at(c) << "\t";
+      }
+      os << endl;
+    }
+  }
+  return os;
 }
 
 int main()
 {
-  Point a{3, 2};
-  cout << "El punto a es: " << a << endl;
-  cout << "Escribe un punto: ";
-  cin >> a;
-  cout << a << endl;
-
-  // open file and rewrite
-  // ofstream miArchivo("archivo.txt", ios::app);
-
-  // if (!miArchivo)
-  // {
-  //   cout << "No se ha creado el archivo" << endl;
-  //   return -1;
-  // }
-
-  // miArchivo << "Estoy guardando en el archivo" << endl;
-  // miArchivo << "El punto es " << a << endl;
-
-  ifstream archivoParaLeer("archivo.txt");
-  if (!archivoParaLeer)
+  cout << "Introduce los 9 valores de la matriz ordenados por filas y columnas:\n";
+  Matrix miMatriz;
+  cin >> miMatriz;
+  // escribir la matriz en un archivo
+  ofstream archivo("data.txt");
+  if (!archivo)
   {
-    cout << "No se ha podido abrir el archivo" << endl;
+    cout << "Error al abrir para escribir\n";
     return -1;
   }
 
-  string line;
-  while (getline(archivoParaLeer, line))
+  archivo << miMatriz << endl;
+
+  archivo.close();
+
+  ifstream archivoLectura("data.txt");
+  if (!archivoLectura)
   {
-    cout << line << endl;
+    cout << "Error al abrir para leer\n";
+    return -1;
   }
 
-  archivoParaLeer.close();
+  archivoLectura >> miMatriz;
 
-  // miArchivo.close();
+  // leer la matriz de un archivo
+  cout << miMatriz;
+  return 1;
 }
