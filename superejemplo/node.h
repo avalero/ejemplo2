07e9fb38c48a9984b2f11265b9af504a3932f1e8
Alberto Valero <alberto.valero@bq.com>
2024-06-27 08:59:14 +0200

    cabecera

--- superejemplo/node.h
+++ superejemplo/node.h
@@ -29,3 +29,15 @@ PNode &push(PNode &head, Student const &st);
  * Iterates over the list (starting at head) and performs f
  */
 void forEach(PNode const &head, LambdaForEach const &f);
+
+/**
+ * Filters the list (starting at head) and returns a new list with the elements that satisfy f
+ * @return the new list
+ * @param head The head of the list
+ * @param f The filter function
+ * @example
+ * auto f = [](Student const &st) { return st.name == "Alberto"; };
+ * auto new_list = filter(head, f);
+ * forEach(new_list, [](Student const &st) { cout << st << endl; });
+ */
+vector<PNode> filter(PNode const &head, function<bool(Student const &)> const &f);
